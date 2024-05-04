a6903ff0a767346dd65ecba756e52f263a272acd
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-04 16:17:22 +0200

    5th added js for header resize controlling

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+const ulHead = document.getElementById('ul_head');
+
+window.addEventListener('resize', function (e) {
+
+    //console.log(window.innerWidth);
+    if (this.window.innerWidth <= 900) {
+        ulHead.classList.add('d-none');
+    }
+    else {
+        if (ulHead.classList.contains('d-none')) {
+            ulHead.classList.remove('d-none');
+        }
+    }
+
+});
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,23 @@
 <header>
     <div class="container d-flex align-items-center  {{Route::currentRouteName() == 'comics.show' ? 'justify-content-center' : 'justify-content-between'}}">
         <a href="/"><img id="logo" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo" /></a>
-        <ul class="{{Route::currentRouteName() == 'comics.create' ? 'd-none' : ''}}">
-            <a href="/" class="{{Route::currentRouteName() == 'comics.show' ? '' : 'd-none'}}" style="font-size: 20px;"><strong><i>Home</i></strong></a>
-            <a href="{{route('comics.index')}}" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">admin</a>
-            <a href="{{route('comics.create')}}" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">create</a>
-            <a href="{{route('json-api')}}" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">json</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">movies</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">tv</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">games</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">collectibles</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">videos</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">fans</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">news</a>
-            <a href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">shop</a>
+        <ul id="ul_head" class="{{Route::currentRouteName() == 'comics.create' ? 'd-none' : ''}}">
+            <a id="head" href="/" class="{{Route::currentRouteName() == 'comics.show' ? '' : 'd-none'}}" style="font-size: 20px;"><strong><i>Home</i></strong></a>
+            <a id="head" href="{{route('comics.index')}}" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">admin</a>
+            <a id="head" href="{{route('comics.create')}}" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">create</a>
+            <a id="head" href="{{route('json-api')}}" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">json</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">movies</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">tv</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">games</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">collectibles</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">videos</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">fans</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">news</a>
+            <a id="head" href="#" class="{{Route::currentRouteName() == 'comics.show' ? 'd-none' : 'd-block'}}">shop</a>
         </ul>
     </div>
 </header>
+
+<style>
+
+</style>
