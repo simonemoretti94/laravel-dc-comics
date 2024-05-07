0fe5b8e551c64553b4552f836aaff2765a78403a
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-07 21:56:11 +0200

    some fixes onto js script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
 
 const ulHead = document.getElementById('ul_head');
-const barsHead = document.getElementById('bars_hide_head');
-const menuHideHead = document.getElementById('menu_hide_head');
+/*const barsHead = document.getElementById('bars_hide_head');
+const menuHideHead = document.getElementById('menu_hide_head');*/
 
 //console.log(ulHead, barsHead, menuHideHead);
 
@@ -11,7 +11,7 @@ window.addEventListener('resize', function (e) {
     console.log(window.innerWidth);
     if (this.window.innerWidth <= 900) {
         ulHead.classList.add('d-none');
-        barsHead.classList.remove('d-none');
+        //barsHead.classList.remove('d-none');
     }
     else {
         if (ulHead.classList.contains('d-none')) {
