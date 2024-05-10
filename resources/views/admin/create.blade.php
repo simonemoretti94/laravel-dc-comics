5fdb2902612ddc5ffe7edd0e8a46f61dc405c75f
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-10 16:16:34 +0200

    7.5th created validation error file into partials

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -9,18 +9,7 @@
 
     {{-- open error --}}
 
-    @if($errors->any())
-
-    <div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-
-        <li>{{ $error }}</li>
-            
-        @endforeach
-    </ul>
-    </div>
-    @endif
+    @include('partials.validation-error')
 
     {{-- close error --}}
 
