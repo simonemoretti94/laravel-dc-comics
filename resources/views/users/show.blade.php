27b59153b64143a27f7d90e7f63c3583cbdab833
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-10 18:10:21 +0200

    8th onto show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,8 +8,88 @@
 <div class="container" style="padding-top: 5rem;">
     {{-- <h1 class="text-primary text-center">{{Route::currentRouteName()}}</h1> --}}
     <h1 class="text-primary text-center">{{$comic->title}}</h1>
-    <p id="p-img" class="mt-2 text-center" ><img src="{{$comic['thumb']}}" alt="{{$comic->title}}"></p>
-    <p class="text-center my-3 p-2 border border-1">{{$comic->description}}</p>
+    <div class="container">
+        <div id="show_row" class="row">
+            <div class="col-6 d-flex justify-content-end">
+                <img width="200" height="300" src="{{$comic['thumb']}}" alt="{{$comic->title}}">
+            </div>
+            <div class="col-6">
+                <p class="border-p">
+                    <span>
+                        <b>
+                        Price: 
+                        </b>
+                    </span>
+                    {{$comic->price}}
+                </p>
+
+                 <p class="border-p">
+                    <span>
+                        <b>
+                        Series:  
+                        </b>
+                    </span>
+                    {{$comic->series}}
+                </p>
+
+                <p class="border-p">
+                    <span>
+                        <b>
+                        Sale_date: 
+                        </b>
+                    </span>
+                    {{$comic->sale_date}}
+                </p>
+
+                <p class="border-p">
+                    <span>
+                        <b>
+                        Type: 
+                        </b>
+                    </span>
+                    {{$comic->type}}
+                </p>
+            </div>
+            <style>
+            div#show_row {
+
+
+                @media screen and (max-width: 450px)
+                {
+
+                & div:nth-child(1) {
+                    > img {
+                        width: 130px;
+                        height: 200px;
+                    }
+                }
+
+                & div:nth-child(2) {
+                    & p {
+                        font-size: x-small;
+                        width: 80%;
+                    }
+                }
+                }
+            }
+
+            .border-p {
+                width: 40%;
+                border-bottom: solid 1px black;
+                padding-bottom: .5rem;
+                font-size: medium;
+            }
+
+            p#p_description {
+                @media screen and (max-width: 450px)
+                {
+                    font-size: small;
+                }
+            }
+            </style>
+        </div>
+    </div>
+    <p id="p_description" class="text-center my-3 p-2 border border-1">{{$comic->description}}</p>
 </div>
 @endsection
 
