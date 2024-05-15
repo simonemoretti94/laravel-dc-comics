ecc49db858d5a446ff5bb1c13f917ed2adb07923
simonemoretti94 <simone.moretti.944@gmail.com>
2024-05-15 21:48:35 +0200

    8.5.1th changed row cards jusitfy content

--- config/comics.php
+++ config/comics.php
@@ -3,7 +3,17 @@
 return [
     'comics' => [
         'title' => 'Blood of Zeus',
-        'description' => 'Blood of Zeus is a Netflix original anime acclaimed by critic',
+
+        'description' => 'Blood of Zeus” is an animated series that blends Greek mythology with epic storytelling. The story unfolds in a world where the gods of Olympus face the threat of the Giants, unleashed by the last Titan’s curse. Amidst this chaos, a commoner named Heron discovers his true lineage as the illegitimate son of Zeus, setting him on a path to save both heaven and earth.
+
+        The narrative weaves through intense battles, divine conspiracies, and the complexities of family ties among gods and mortals. Heron’s journey is fraught with challenges as he learns to harness his powers and confronts his destiny. The gods, led by Zeus, are depicted with their iconic traits and powers, while the Giants serve as formidable adversaries, embodying the primal chaos that the gods strive to quell.
+        
+        Intrigue and betrayal are central themes, as the pantheon of gods is not immune to corruption and power struggles. The series delves into the relationships between characters, such as the bond between Heron and his divine father, Zeus, and the manipulative schemes of Hera.
+        
+        The animation style is rich and detailed, capturing the grandeur of the mythological world. The action sequences are dynamic, showcasing the might of the gods and the ferocity of the Giants. The series also explores the human aspect of these myths, bringing a sense of relatability to the larger-than-life figures.
+
+        “Blood of Zeus” stands out for its fresh take on classic tales, reimagining the ancient myths with a modern sensibility that appeals to both newcomers to Greek mythology and long-time enthusiasts. It’s a tale of heroism, sacrifice, and the enduring struggle between order and chaos',
+
         'thumb' => 'https://th.bing.com/th/id/R.deeab8ccfaf9a32ea7632c88aa40f8c9?rik=hnYTWzCq6bviYw&pid=ImgRaw&r=0',
         'price' => '21.57',
         'series' => 'Netflix anime',
