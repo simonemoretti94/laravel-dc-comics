fcdf2f1803a4dffa5692b10b4f5d2b0ee073fd69
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-06 14:10:00 +0200

    9.2.5th

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,25 +13,27 @@
             <div class="col-6 d-flex justify-content-end">
                 {{-- <img width="200" height="300" src="{{$comic['thumb']}}" alt="{{$comic->title}}"> --}}
                 @if(Str::startsWith($comic->thumb , 'https://'))
-                <img id="thumb_img" src="{{$comic->thumb}}" alt="{{$comic->title}}" class="img-fluid rounded-top"  width="200" height="300" >
+                <img id="thumb_img" src="{{$comic->thumb}}" alt="{{$comic->title}}" class="img-fluid rounded-top"
+                    width="200" height="300">
                 @else
-                    <img id="thumb_img" src="{{asset('storage/' . $comic->thumb)}}" alt="{{$comic->title}}" class="img-fluid rounded-top"  width="200" height="300" >
+                <img id="thumb_img" src="{{asset('storage/' . $comic->thumb)}}" alt="{{$comic->title}}"
+                    class="img-fluid rounded-top" width="200" height="300">
                 @endif
             </div>
             <div class="col-6">
                 <p class="border-p">
                     <span>
                         <b>
-                        Price: 
+                            Price:
                         </b>
                     </span>
                     {{$comic->price}}
                 </p>
 
-                 <p class="border-p">
+                <p class="border-p">
                     <span>
                         <b>
-                        Series:  
+                            Series:
                         </b>
                     </span>
                     {{$comic->series}}
@@ -40,60 +42,75 @@
                 <p class="border-p">
                     <span>
                         <b>
-                        Sale date: 
+                            Sale date:
                         </b>
                     </span>
                     {{$comic->sale_date}}
                 </p>
 
+                @if($comic->types)
                 <p class="border-p">
                     <span>
                         <b>
-                        Type: 
+                            Type:
+                        </b>
+                    </span>
+                    @foreach ($comic->types as $temp)
+                    <span>{{ $temp->name }}
+                        @if(!$loop->last)
+                        ,
+                        @endif
+                    </span>
+                    @endforeach
+                </p>
+                @else
+                <p class="border-p">
+                    <span>
+                        <b>
+                            Type:
                         </b>
                     </span>
                     {{$comic->type}}
                 </p>
+                @endif
             </div>
             <style>
-            div#show_row {
+                div#show_row {
 
 
-                @media screen and (max-width: 450px)
-                {
+                    @media screen and (max-width: 450px) {
 
-                & div:nth-child(1) {
-                    > img {
-                        width: 130px;
-                        height: 200px;
+                        & div:nth-child(1) {
+                            >img {
+                                width: 130px;
+                                height: 200px;
+                            }
+                        }
+
+                        & div:nth-child(2) {
+                            & p {
+                                font-size: x-small;
+                                width: 80%;
+                            }
+                        }
                     }
                 }
 
-                & div:nth-child(2) {
-                    & p {
-                        font-size: x-small;
-                        width: 80%;
+                .border-p {
+                    width: 40%;
+                    border-bottom: solid 1.5px black;
+                    border-left: solid 1.5px black;
+                    padding-bottom: .5rem;
+                    padding-left: .2rem;
+                    font-size: medium;
+                    box-shadow: -1px 3px black;
+                }
+
+                p#p_description {
+                    @media screen and (max-width: 450px) {
+                        font-size: small;
                     }
                 }
-                }
-            }
-
-            .border-p {
-                width: 40%;
-                border-bottom: solid 1.5px black;
-                border-left: solid 1.5px black;
-                padding-bottom: .5rem;
-                padding-left: .2rem;
-                font-size: medium;
-                box-shadow: -1px 3px black;
-            }
-
-            p#p_description {
-                @media screen and (max-width: 450px)
-                {
-                    font-size: small;
-                }
-            }
             </style>
         </div>
     </div>
@@ -111,7 +128,6 @@
         width: 200px;
         height: 300px;
         filter: drop-shadow(0 0 10px black)
-       
     }
 </style>
 @endsection
